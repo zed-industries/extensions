896a629c20af153a6978f84dbbb05c960138a8d8
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-03-20 13:46:17 -0700

    Use zed-extension CLI for packaging extensions
    Co-authored-by: Marshall <marshall@zed.dev>

--- src/lib/validation.js
+++ src/lib/validation.js
@@ -1,16 +1,3 @@
-import ajvModule from "ajv";
-import { readJsonFile } from "./fs.js";
-
-const Ajv = ajvModule.default;
-
-const ajv = new Ajv({});
-const themeValidator = ajv.compile(
-  await readJsonFile("schemas/theme-family.json"),
-);
-const languageConfigValidator = ajv.compile(
-  await readJsonFile("schemas/language-config.json"),
-);
-
 /**
  * Exceptions to the rule of extension IDs ending in `-zed`.
  *
@@ -54,38 +41,5 @@ export function validateManifest(manifest) {
     throw new Error(
       `Extension names should not end with " Zed", as they are all Zed extensions: "${manifest["name"]}".`,
     );
-  }
-
-  const authors = manifest["authors"];
-  if (!authors) {
-    throw new Error(`Missing "authors" field.`);
-  }
-
-  if (!Array.isArray(authors)) {
-    throw new Error(`"authors" field must be an array of strings.`);
-  }
-
-  if (authors.some((author) => typeof author !== "string")) {
-    throw new Error(`"authors" field must be an array of strings.`);
-  }
-}
-
-/**
- * @param {Record<string, any>} config
- */
-export function validateLanguageConfig(config) {
-  languageConfigValidator(config);
-  if (languageConfigValidator.errors) {
-    throw new Error(ajv.errorsText(languageConfigValidator.errors));
-  }
-}
-
-/**
- * @param {Record<string, any>} theme
- */
-export function validateTheme(theme) {
-  themeValidator(theme);
-  if (themeValidator.errors) {
-    throw new Error(ajv.errorsText(themeValidator.errors));
   }
 }
--- src/lib/validation.test.js
+++ src/lib/validation.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { validateManifest, validateTheme } from "./validation.js";
+import { validateManifest } from "./validation.js";
 
 describe("validateManifest", () => {
   describe("given a valid manifest", () => {
@@ -22,68 +22,6 @@ describe("validateManifest", () => {
         validateManifest({ name: "Zed Something" }),
       ).toThrowErrorMatchingInlineSnapshot(
         `[Error: Extension names should not start with "Zed ", as they are all Zed extensions: "Zed Something".]`,
-      );
-    });
-  });
-
-  describe("when the `authors` field is missing", () => {
-    it("throws a validation error", () => {
-      expect(() =>
-        validateManifest({ name: "My Cool Extension", version: "1.0.0" }),
-      ).toThrowErrorMatchingInlineSnapshot(`[Error: Missing "authors" field.]`);
-    });
-  });
-
-  describe("when the `authors` field has a string value", () => {
-    it("throws a validation error", () => {
-      expect(() =>
-        validateManifest({
-          name: "My Cool Extension",
-          version: "1.0.0",
-          authors: "Me",
-        }),
-      ).toThrowErrorMatchingInlineSnapshot(
-        `[Error: "authors" field must be an array of strings.]`,
-      );
-    });
-  });
-
-  describe("when the `authors` field is an array with non-string values", () => {
-    it("throws a validation error", () => {
-      expect(() =>
-        validateManifest({
-          name: "My Cool Extension",
-          version: "1.0.0",
-          authors: ["Me", 1, { name: "Me" }],
-        }),
-      ).toThrowErrorMatchingInlineSnapshot(
-        `[Error: "authors" field must be an array of strings.]`,
-      );
-    });
-  });
-});
-
-describe("validateTheme", () => {
-  describe("given a valid theme", () => {
-    it("does not throw", () => {
-      const minimalValidTheme = {
-        name: "My Valid Theme",
-        author: "Me",
-        themes: [],
-      };
-
-      expect(() => validateTheme(minimalValidTheme)).not.toThrow();
-    });
-  });
-
-  describe("given an invalid theme", () => {
-    it("throws a validation error", () => {
-      expect(() =>
-        validateTheme({
-          name: "My Invalid Theme",
-        }),
-      ).toThrowErrorMatchingInlineSnapshot(
-        `[Error: data must have required property 'author']`,
       );
     });
   });
