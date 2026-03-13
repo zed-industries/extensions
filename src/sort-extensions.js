e7b8f92863994f6927569fc14994a9a1e349bbbf
Akila Madhushanka (KilerZack) <developer.akila5@gmail.com>
2026-03-13 18:27:01 +0530

    Add .gitmodules for Laravel extension

--- src/sort-extensions.js
+++
@@ -1,5 +0,0 @@
-import { sortExtensionsToml } from "./lib/extensions-toml.js";
-import { sortGitmodules } from "./lib/git.js";
-
-await sortExtensionsToml("extensions.toml");
-await sortGitmodules(".gitmodules");
